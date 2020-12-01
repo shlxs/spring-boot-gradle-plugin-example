535daef0c76944e1fe7c7c570f30e635887174da
邵宏亮 <shaohongliang@dobest.com>
2020-12-01 17:27:40 +0800

    commit

---
+++ src/main/java/io/shlxs/examples/LombokPojo.java
@@ -0,0 +1,12 @@
+package io.shlxs.examples;
+
+import lombok.Data;
+
+/**
+ * @author shaohongliang
+ * @since 2020/12/1 16:13
+ */
+@Data
+public class LombokPojo {
+    private String a;
+}
---
+++ src/main/java/io/shlxs/examples/SpringBootMain.java
@@ -0,0 +1,8 @@
+package io.shlxs.examples;
+
+class SpringBootMain{
+
+    public static void main(String[] args){
+        new LombokPojo().setA("a");
+    }
+}
